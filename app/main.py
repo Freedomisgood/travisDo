ee3922ab617916935758b66575b1814e1b59720a
Freedomisgood <1063052964@qq.com>
2021-10-23 23:59:23 +0800

    recorrect

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import datetime
 
 
 def printTimeNow():
-	return datetime.datetime.now()
+    return datetime.datetime.now()
 
 
 if __name__ == '__main__':
-	res = printTimeNow()
-	print(res)
+    res = printTimeNow()
+    print(res)
